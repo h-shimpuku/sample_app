ab9dbe3906cbbb139852885edff618a781dc9b01
h-shimpuku <h-shimpuku@wwwave.jp>
2021-11-22 01:35:27 +0000

    last exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -83,6 +83,14 @@ class User < ApplicationRecord
                      OR user_id = :user_id", user_id: id)
   end
 
+  # ユーザーのステータスフィードを返す
+  # def feed
+  #   part_of_feed = "relationships.follower_id = :id or microposts.user_id = :id"
+  #   Micropost.left_outer_joins(user: :followers)
+  #            .where(part_of_feed, { id: id }).distinct
+  #            .includes(:user, image_attachment: :blob)
+  # end
+
   # ユーザーをフォローする
   def follow(other_user)
     following << other_user
